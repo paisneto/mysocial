ecc4ffb63f6af5abec498ae70a17c15b1cc311c2
ernestocasanova <ernestonet@msn.com>
2017-07-30 17:01:39 +0100

    new edit moment detail

--- Mysocial/app/src/main/java/com/example/nunocoelho/mysocial/moment/EditMommentActivity.java
+++ Mysocial/app/src/main/java/com/example/nunocoelho/mysocial/moment/EditMommentActivity.java
@@ -2,6 +2,7 @@ package com.example.nunocoelho.mysocial.moment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.nunocoelho.mysocial.R;
 
@@ -11,5 +12,18 @@ public class EditMommentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_momment);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Edit Moment");
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
